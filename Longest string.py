e47f7db29bb64de405cb448096cdc1f32f026be7
Shalom91 <56162445+Shalom91@users.noreply.github.com>
2019-12-06 10:11:11 +0200

    Update Longest string.py

--- Longest string.py
+++ Longest string.py
@@ -1,16 +1,15 @@
-my_list = ['blood', 'air', 'fire', 'yuyuy']
+my_list = ['blood', 'air', 'fire', 'tutor']
 
 def longest_string(word_list):
     longest_len = 0
     longest_words = []
-    for s in my_list:
-        if len(s) > longest_len:
-            longest_len = len(s)
-            longest_words = [s]
-        elif len(s) == longest_len:
-            longest_words.append(s)
-        print(longest_words)
+    for word in my_list:
+        if len(word) > longest_len:
+            longest_len = len(word)
+            longest_words = [word]
+        elif len(word) == longest_len:
+            longest_words.append(word)
+    return longest_words
 
-longest_string(my_list)
-
+print(longest_string(my_list))
       
